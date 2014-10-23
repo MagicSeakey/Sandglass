baf81098b25fc1618a48be2c8a8b6e9d8ad9c75f
Lee Seakey <sea@seadeMacBook-Pro.local>
2014-10-23 17:02:36 +0800

    hi

---
+++ AViewController.h
@@ -0,0 +1,13 @@
+//
+//  AViewController.h
+//  Sandglass
+//
+//  Created by sea on 14/10/23.
+//  Copyright (c) 2014年 seakey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AViewController : UIViewController
+
+@end
